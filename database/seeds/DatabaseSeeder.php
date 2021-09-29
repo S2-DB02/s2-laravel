44e7fe546e6231e2cb685cb395cfc7c57c5e5411
Mohamad,Anas A <a.mohamad@student.fontys.nl>
2021-09-29 12:12:22 +0200

    Userseeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(UsersTableSeeder::class);
+        $this->call(UserSeeder::class);
 
 
 //        for($i = 0; $i < 1000; $i++) {
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
+use Faker\Factory as Faker;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $faker = Faker::create('App\User');
+        for($i=0; $i<=100; $i++):
+        User::make([
+            'name' => $faker->name,
+            'email' => $faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            ])->save();
+    endfor;
+        
+    }
+}
