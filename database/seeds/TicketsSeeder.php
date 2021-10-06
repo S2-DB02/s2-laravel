40dd9b86014e3b19e9319c35965b080bd3d43de4
Faizal Susetyo <456826@student.fontys.nl>
2021-10-06 13:45:08 +0200

    Added Ticket Seeder

--- database/seeds/TicketsSeeder.php
+++ database/seeds/TicketsSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\ticket;
+use Faker\Factory as Faker;
+
 
 class TicketsSeeder extends Seeder
 {
@@ -12,5 +15,16 @@ class TicketsSeeder extends Seeder
     public function run()
     {
         //
+        $faker = Faker::create('App\ticket');
+        for($i=0; $i<=100; $i++):
+        ticket::make([
+            'name' => $faker->word,
+            'photo' => $faker->imageUrl(640, 480, 'cats'),
+            'remark' => $faker->text,
+            'status' => $faker->randomElement($array = array ( 1, 2, 3 )),
+            'type' => $faker->randomElement($array = array (1, 2, 3, 4, 5 )),
+            'user_id' => $faker->randomDigitNotNull,
+            ])->save();
+    endfor;
     }
 }
