7e302029c77a6a7fb19bea980206ee882867f18f
Jeffrey Derksen <470568@student.fontys.nl>
2021-10-21 12:41:31 +0200

    Initial work on user get api.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Resources\TicketResource;
+use App\Http\Resources\UserResource;
 use App\ticket;
+use App\User;
 use Illuminate\Http\Request;
 
 /*
@@ -33,6 +35,11 @@ Route::get('/ticket/{ticket}', 'TicketController@show');
 // {
 //     dd(ticket::find($ticket));
 //     return TicketResource::collection(ticket::find($ticket));
+// });
+
+Route::get('/user/{user}', 'GebruikersController@show');
+// Route::get('/user', function () {
+//     return new UserResource(User::find(1));
 // });
 
 Route::post('/ticket', 'TicketController@store');
